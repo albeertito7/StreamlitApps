b4c809c435797d4dbc58fa7abe2352127cb61581
albeertito7 <albertperezdatsira@gmail.com>
2021-01-16 23:21:23 +0100

    Update myapp.py

--- app1/myapp.py
+++ app1/myapp.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-## Simple Stock Price App
+# Simple Stock Price App
 Shown are the stock **closing price** and ***volume*** of Google!
 """)
 
@@ -16,5 +16,12 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
